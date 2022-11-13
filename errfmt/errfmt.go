222858f77f7292db55b167914a86471845110e19
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-11-13 15:06:33 +0900

    Add errfmt package

---
+++ errfmt/errfmt.go
@@ -0,0 +1,19 @@
+// Package errfmt format the error message.
+package errfmt
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Wrap return wrapping error with message.
+// If e is nil, return new error with msg. If msg is empty string, return e.
+func Wrap(e error, msg string) error {
+	if e == nil {
+		return errors.New(msg)
+	}
+	if msg == "" {
+		return e
+	}
+	return fmt.Errorf("%s: %w", msg, e)
+}
